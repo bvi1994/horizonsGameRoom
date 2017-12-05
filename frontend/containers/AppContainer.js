4dd4e6cc977c3d4d172584da1a352171c21abd76
bvi1994 <bvi1@ucsc.edu>
2017-12-04 17:01:03 -0800

    Fixed typo

--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 // import Title from '../components/Title';
-import LoginForm from '../components/LoginForm'
+import LoginForm from '../components/LoginForm';
 
 const AppContainer = (/*{ name }*/) => {
     return (
