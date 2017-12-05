1555d99a40a3f18096ff544a2b4c08317b229d4b
borajimin <borajimin@gmail.com>
2017-12-04 21:43:34 -0800

    Added some console

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 
 app.use(api(passport));
 
-app.listen(PORT, error => {
+app.listen(process.env.PORT, error => {
     error
     ? console.error(error)
     : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
