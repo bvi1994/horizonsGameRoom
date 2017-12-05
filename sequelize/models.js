1806548fb5b76c8a5eb534ada45bf5fc2a120258
borajimin <borajimin@gmail.com>
2017-12-05 14:48:23 -0800

    Still working on database connection

--- sequelize/models.js
+++ sequelize/models.js
@@ -7,8 +7,6 @@ if(process.env.HEROKU_POSTGRESQL_AMBER_URL) {
     sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_AMBER_URL, {
         dialect:  'postgres',
         protocol: 'postgres',
-        port:     match[4],
-        host:     match[3],
         logging:  true
     });
 } else {
