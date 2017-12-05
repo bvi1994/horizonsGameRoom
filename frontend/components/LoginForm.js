4bb9d527d2b1037c897fe7126b1720ccfd78886a
bvi1994 <bvi1@ucsc.edu>
2017-12-05 15:17:03 -0800

    banana '

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,8 +1,34 @@
 import React, {Component} from "react";
+import axios from 'axios';
 
 class LoginForm extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+    onUsernameChange(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+    onPasswordChange(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+    login() {
+        axios.post('http://localhost:3000/register', {
+            username: this.state.username,
+            password: this.state.password,
+            firstName: this.state.firstName
+        }, {
+            withCredentials: true
+        })
+      .then(() => this.setState({redirect: true}))
+      .catch((err) => {console.log('Register Post request failed', err);});
     }
     render() {
         return(
@@ -13,16 +39,16 @@ class LoginForm extends Component {
               </div>
               <div id="userNameField">
                 Username: <br />
-                <input type="text" name="firstName" placeholder="Username"/>
+                <input type="text" onChange={e => this.onUsernameChange(e)} name="firstName" placeholder="Username"/>
               </div>
               <div id="passwordField">
                 Password: <br />
-                <input type="password" name="userName" placeholder="Password" />
+                <input type="password" onChange={e => this.onPasswordChange(e)} name="password" placeholder="Password" />
               </div>
               <div id="loginButton">
-                <button>
+                <button onClick={() => this.login()}><a href="/auth/test">
                   Login
-                </button>
+                </a></button>
               </div>
               <div id="githubSignUp">
                 <button>
