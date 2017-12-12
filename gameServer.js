80e666cf6de30a9add1bcd8732f12fff32eeb36e
borajimin <borajimin@gmail.com>
2017-12-12 14:45:45 -0800

    Please work

--- gameServer.js
+++ gameServer.js
@@ -2,21 +2,20 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-module.exports = () => {
-    router.use((req, res, next) => {
-        if (!req.user) {
-            res.status(401).json({success: 'failed'});
-        } else {
-            next();
-        }
-    });
 
-    router.get('/create/:game', (req, res) => {
-        console.log("create game route reached");
-        if(req.params.game === 'slapjack') {
-            res.sendFile(__dirname + '/public/slapjack.html');
-        }
-    });
+router.use((req, res, next) => {
+    if (!req.user) {
+        res.status(401).json({success: 'failed'});
+    } else {
+        next();
+    }
+});
 
-    return router;
-};
+router.get('/create/:game', (req, res) => {
+    console.log("create game route reached");
+    if(req.params.game === 'slapjack') {
+        res.sendFile(__dirname + '/public/slapjack.html');
+    }
+});
+
+module.exports = router;
