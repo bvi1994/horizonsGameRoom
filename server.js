7d5bce628dbc23fbc0b6cc289d63d9937472a895
borajimin <borajimin@gmail.com>
2017-12-12 14:48:45 -0800

    Heroku fix

--- server.js
+++ server.js
@@ -92,8 +92,8 @@ passport.use(new GitHubStrategy({
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(game);
 app.use(api(passport));
+app.use(game);
 
 var server = app.listen(PORT, error => {
     error
