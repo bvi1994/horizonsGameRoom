140f51475880711c874c14b2062d2bf90e094f07
borajimin <borajimin@gmail.com>
2017-12-13 13:52:54 -0800

    Please work

--- frontend/components/Ready.js
+++ frontend/components/Ready.js
@@ -66,7 +66,7 @@ function createScene() {
 	  renderer.shadowMap.enabled = true;
 
 	// Add the Renderer to the DOM, in the world div.
-	  container = document.getElementById('world').appendChild(renderer.domElement);
+	  container = document.body.appendChild(renderer.domElement);
 
 	// RESPONSIVE LISTENER
 	  window.addEventListener('resize', handleWindowResize, false);
