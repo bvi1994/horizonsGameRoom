695d16c143c0a81b1cfb586dc0dd7c16ab60bd2f
borajimin <borajimin@gmail.com>
2017-12-13 14:05:35 -0800

    id is world

--- frontend/components/Ready.js
+++ frontend/components/Ready.js
@@ -622,7 +622,7 @@ function init(event) {
 	loop();
 }
 
-window.addEventListener('load', init, false);
+document.getElementById('world').addEventListener('load', init, false);
 
 class Ready extends Component {
     constructor(props) {
