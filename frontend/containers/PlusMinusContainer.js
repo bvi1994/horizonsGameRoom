b3329e2d2c7c77398739ca38e88550f1529e030c
borajimin <borajimin@gmail.com>
2017-12-14 16:42:00 -0800

    Work

--- frontend/containers/PlusMinusContainer.js
+++ frontend/containers/PlusMinusContainer.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { connect } from 'react-redux';
 import { HashRouter, Route } from 'react-router-dom';
 import PlusMinus from '../components/PlusMinus/PlusMinus';
 const PlusMinusContainer = () => {
     return (
+      <MuiThemeProvider>
         <HashRouter>
           <div style={{height: "100%"}}>
             <Route exact path="/" component={PlusMinus} />
           </div>
         </HashRouter>
+      </MuiThemeProvider>
     );
 };
 
