e72be4c067e569285cfd914c8d0f30d578bf8fa1
borajimin <borajimin@gmail.com>
2017-12-15 22:54:01 -0800

    autofocus to the first input field

--- frontend/components/PlusMinus/PlusMinus.js
+++ frontend/components/PlusMinus/PlusMinus.js
@@ -88,6 +88,7 @@ class PlusMinus extends Component {
             if(ReactDOM.findDOMNode(this.nextComponent[i + 1]) === null) {
                 this.nextComponent.forEach(nc => {nc.value = '';});
                 this.makeQuestions(this.state.level);
+                ReactDOM.findDOMNode(this.nextComponent[0]).focus();
             } else {
                 ReactDOM.findDOMNode(this.nextComponent[i + 1]).focus();
             }
@@ -106,7 +107,7 @@ class PlusMinus extends Component {
               {
                 this.state.questions.map((question, i) => {
                     return (<h1 key={i}>{question.first} {this.operators[question.operator]} {question.second} =
-                      <input className="input-field" autoFocus={i === 0 ? true : false} ref={c => {this.nextComponent[i] = c;}} key={i} onChange={e => this.answer(e, i)}/></h1>);
+                      <input className="input-field" ref={c => {this.nextComponent[i] = c;}} key={i} onChange={e => this.answer(e, i)}/></h1>);
                 })
               }
             </ol>
