7955e60e529df31888059a99913148a2b125744d
bvi1994 <bvi1@ucsc.edu>
2017-12-18 14:49:13 -0800

    fix issues

--- frontend/components/Chatbox.js
+++ frontend/components/Chatbox.js
@@ -13,33 +13,27 @@ class Chatbox extends Component {
         this.state = {
             socket: socket,
             roomName: "Main Chat Room",
-            username: "",
+            username: props.user.username,
             user: ""
         };
     }
     componentDidMount() {
-        this.setState({
-            username: this.props.user.username
-        });
-        this.state.socket.on('connect', () => {
-            this.state.socket.emit('username', this.state.username);
-            this.state.socket.emit('room', this.state.roomName);
-        });
+        this.state.socket.emit('username', this.props.user.username);
+        this.state.socket.emit('room', "Main Chat Room");
         this.state.socket.on('errorMessage', message => {
-            console.log("Unable to connect. Error: ", message);
+          console.log("Unable to connect. Error: ", message);
         });
     }
     componentWillReceiveProps(props) {
-        this.setState({
-            username: props.user.username,
-            user: props.user
-        });
+      this.setState({
+        username: props.user.username,
+        user: props.user
+      });
     }
     render() {
         return(
           <div id="chatBox" className="section">
               Horizons Playground Chat Room 🌎
-
               <ChatWindow user={this.state.user} socket={this.state.socket} />
           </div>
         );
---
+++ frontend/components/GameSession.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from "react";
+import '../assets/stylesheets/GameSession.css';
+import JoinButton from './JoinButton.js';
+import WatchButton from './WatchButton.js';
+import Avatar from 'material-ui/Avatar';
+
+class GameSession extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="roomBlock">
+                {/* <img className="" alt="Pikachu gif" src="https://media.giphy.com/media/OgsZnIsoPkCfS/giphy.gif"/> */}
+                <div className="roomName">
+                  <h3>Game Room Name</h3>
+                </div>
+                {/* <h3>{this.state.GameRoomName}</h3> */}
+                <div className="gameInfo">
+                  <div id="playerList">
+                    <div className="player">Pikachu</div>
+                    <div className="player">Pichu</div>
+                  </div>
+                  <div className="playerInfo">
+                    <div id="playerListAvatar">
+                      <Avatar src="https://i.imgur.com/x6PRlCE.png" size={25} />
+                      <Avatar src="https://i.imgur.com/x6PRlCE.png" size={25} />
+                    </div>
+                  </div>
+                </div>
+                <div id="menuOptions">
+                  <div id="joinButton">
+                    <JoinButton />
+                  </div>
+                  <div id="watchButton">
+                    <WatchButton />
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GameSession;
