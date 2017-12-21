503fe0fe9c069459ddd98eadcfc80eb4611db774
borajimin <borajimin@gmail.com>
2017-12-21 00:28:33 -0800

    Work

--- frontend/components/Dashboard.js
+++ frontend/components/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Redirect } from 'react-router';
 import Profile from './Profile';
 import axios from 'axios';
 import Chatbox from './Chatbox.js';
@@ -12,7 +11,6 @@ class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirect: false,
             user: {},
             gameSession: [],
             chatOpen: false
@@ -44,13 +42,20 @@ class Dashboard extends Component {
         SOCKET.emit('room', "Main Chat Room");
     }
     render() {
-        return (this.state.redirect) ? <Redirect to="/" /> : (
+        return (
         <div style={{height: "100%"}}>
             <div id="mainDashboard" style={{minHeight: "100%"}}>
                 <Profile user={this.state.user} addGame={this.addGameSession} joinRoom={() => this.joinRoom()}/>
                 <CurrentGameSession session={this.state.gameSession}/>
                 <Drawer anchor="right" open={this.state.openChat} onClose={this.toggleDrawer(false)}>
-                    <Chatbox user={this.state.user} socket={SOCKET} />
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        onClick={this.toggleDrawer('right', false)}
+                        onKeyDown={this.toggleDrawer('right', false)}
+                    >
+                        <Chatbox user={this.state.user} socket={SOCKET} />
+                    </div>
                 </Drawer>
             </div>
           </div>
