7b0dae25a664a03b1cdb5d2abc78cdf55788799c
borajimin <borajimin@gmail.com>
2017-12-20 16:06:06 -0800

    work

--- frontend/components/PlusMinus/PlusMinus.js
+++ frontend/components/PlusMinus/PlusMinus.js
@@ -206,7 +206,7 @@ class PlusMinus extends Component {
               {
                 this.state.questions.map((question, i) => {
                     return (<h3 key={i}>{question.first} {this.operators[question.operator]} {question.second} =
-                      <input className="input-field" ref={c => {this.nextComponent[i] = c;}} key={i} value={this.state.answers[i]} onChange={e => this.answer(e, i)}/></h3>);
+                      <input className="input-field" ref={c => {this.nextComponent[i] = c;}} key={i} value={this.state.answers[i]} onKeyUp={e => this.answer(e, i)}/></h3>);
                 })
               }
             </ol>
